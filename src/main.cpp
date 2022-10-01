573296a1314858ac089b88e5b2772ea40314c056
Skkay <skkay.thomas@gmail.com>
2022-10-01 12:06:17 +0200

    Add Arduino main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
 
+int inputPin = A0;
+int outputPin = 9;
+
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+
+    pinMode(inputPin, INPUT);
+    pinMode(outputPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    int isOn = digitalRead(inputPin);
+    Serial.print("Healthcheck... ");
+    Serial.println(isOn == HIGH ? "ON" : "OFF");
+
+    if (isOn == LOW)
+    {
+        Serial.println("Server is OFF, sending digitalWrite");
+        digitalWrite(outputPin, HIGH);
+        delay(1000);
+        digitalWrite(outputPin, LOW);
+    }
+
+    delay(5000);
 }
